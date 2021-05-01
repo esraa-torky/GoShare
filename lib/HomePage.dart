acb1c3e0bcf996dbf9b8abd1340eff6d19b595ee
Ekin Nohutçu <60942915+ekinnohutcu@users.noreply.github.com>
2021-05-01 11:52:55 +0300

    bottom navigation bar added into homepage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,12 +1,93 @@
-import 'package:flutter/material.dart';
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+/// Flutter code sample for BottomNavigationBar
 
-class _HomePageState extends State<HomePage> {
+// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
+// widget. The [BottomNavigationBar] has three [BottomNavigationBarItem]
+// widgets, which means it defaults to [BottomNavigationBarType.fixed], and
+// the [currentIndex] is set to index 0. The selected item is
+// amber. The `_onItemTapped` function changes the selected item's index
+// and displays a corresponding message in the center of the [Scaffold].
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const HomePage());
+
+/// This is the main application widget.
+class HomePage extends StatelessWidget {
+  const HomePage({key}) : super(key: key);
+
+  static const String _title = 'GoShare';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      title: _title,
+      home: Home(),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class Home extends StatefulWidget {
+  const Home({key}) : super(key: key);
+
+  @override
+  _Home createState() => _Home();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _Home extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Profile',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Maps',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GoShare'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Maps',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
